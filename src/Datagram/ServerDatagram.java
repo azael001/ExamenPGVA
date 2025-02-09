5496008db06ea631ee6ee20650b67da026f07d52
Azael <azael.carballo@gmail.com>
2025-02-09 11:16:59 +0000

    a

--- src/Datagram/ServerDatagram.java
+++ src/Datagram/ServerDatagram.java
@@ -3,34 +3,58 @@ package Datagram;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ServerDatagram extends Thread {
     private static DatagramSocket datagramSocket = null;
+    public static String direccionIPGrupo = "225.10.10.10";
+    public static int puerto = 6998;
+    public static MulticastSocket socketMulticast = null;
+    public static InetSocketAddress grupo = null;
+    public static NetworkInterface netIf = null;
     private static ArrayList<InetAddress> arrayDir = new ArrayList<>();
     private static ArrayList<Integer> arrayPuertos = new ArrayList<>();
     private static ArrayList<String> arrayNombres = new ArrayList<>();
     public static final int privado = 3;
     public static final int enviar = 2;
+    public static final int recibir = 1;
     public int tipoHilo = -1;
-
     private static boolean ejecucion = true;
+    public static String nombre = "";
+
+
+    private static void pedirNombre() {
+        Scanner name = new Scanner(System.in);
+        System.out.print("Escribe de nombre: ");
+        nombre = name.nextLine();
+    }
     public ServerDatagram(int tipoHilo) {
         this.tipoHilo = tipoHilo;
     }
     public static void main(String[] args) {
         try {
             ejecucion = true;
+            pedirNombre();
+            socketMulticast = new MulticastSocket(puerto);
+            InetAddress dir = InetAddress.getByName(direccionIPGrupo);
+            grupo = new InetSocketAddress(dir, puerto);
+            netIf = NetworkInterface.getByInetAddress(dir);
+            socketMulticast.joinGroup(grupo, netIf);
             ServerDatagram privadoC = new ServerDatagram(privado);
             ServerDatagram enviarC = new ServerDatagram(enviar);
+            ServerDatagram recibirC = new ServerDatagram(recibir);
             enviarC.start();
             privadoC.start();
+            recibirC.start();
             enviarC.join();
             privadoC.join();
+            recibirC.join();
+
+            socketMulticast.leaveGroup(grupo, netIf);
+            socketMulticast.close();
+            System.out.println("Socket cerrado");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -45,6 +69,9 @@ public class ServerDatagram extends Thread {
             case privado:
                 privadoDatagram();
                 break;
+            case recibir:
+                runRecibir();
+                break;
         }
     }
 
@@ -58,7 +85,6 @@ public class ServerDatagram extends Thread {
         Thread escuchaMensajes = new Thread(this::escucharMensajes);
         escuchaMensajes.start();
 
-        System.out.println("Servidor UDP en escucha...");
     }
 
     private void escucharMensajes() {
@@ -98,7 +124,7 @@ public class ServerDatagram extends Thread {
                     ejecucion = false;
                     break;
                 }
-                if(msg[0].equals("Privado")){
+                if(msg[0].startsWith("Privado")){
                     int x = posicionamientoNombre(msg[1]);
                     if(x==9999){
                         System.out.println("Persona no encontrada");
@@ -111,12 +137,34 @@ public class ServerDatagram extends Thread {
                         datagramSocket.send(datagrama2);
                     }
                 }
+                else{
+
+                    mensaje = nombre + ":" + mensaje;
+                    DatagramPacket paquete = new DatagramPacket(mensaje.getBytes(), mensaje.length(), grupo.getAddress(), puerto);
+                    socketMulticast.send(paquete);
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
+    //Multicast
+    private void runRecibir() {
+        final int tamanoBufferMensaje = 1024;
+        String mensaje;
+        byte[] bufferMensajeRecibido = new byte[tamanoBufferMensaje];
 
+        while (ejecucion) {
+            try {
+                DatagramPacket paquete = new DatagramPacket(bufferMensajeRecibido, bufferMensajeRecibido.length);
+                socketMulticast.receive(paquete);
+                mensaje = new String(paquete.getData(), 0, paquete.getLength());
+                System.out.println(mensaje);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
     private static int posicionamientoNombre(String nombreUser) {
         for (int i = 0; i < arrayNombres.size(); i++) {
             if (arrayNombres.get(i).equals(nombreUser)) {
--- src/Datagram/UserDatagram.java
+++ src/Datagram/UserDatagram.java
@@ -1,14 +1,22 @@
 package Datagram;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 import java.util.Scanner;
 
 public class UserDatagram extends Thread {
+    public static String direccionIPGrupo = "225.10.10.10";
+    public static int puerto = 6998;
+    public static MulticastSocket socketMulticast = null;
+    public static InetSocketAddress grupo = null;
+    public static NetworkInterface netIf = null;
     public static String nombre = "";
     private static boolean ejecucion = true;
     public int tipoHilo = -1;
     public static final int ENVIAR = 1;
+    public static final int RECIBIR = 2;
     public static final int PRIVADO = 3;
     public UserDatagram(int tipoHilo) {
         this.tipoHilo = tipoHilo;
@@ -24,14 +32,30 @@ public class UserDatagram extends Thread {
         try {
             ejecucion = true;
             pedirNombre();
+            try {
+                socketMulticast = new MulticastSocket(puerto);
+                InetAddress dir = InetAddress.getByName(direccionIPGrupo);
+                grupo = new InetSocketAddress(dir, puerto);
+                netIf = NetworkInterface.getByInetAddress(dir);
+                socketMulticast.joinGroup(grupo, netIf);
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            } catch (SocketException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             UserDatagram enviarC = new UserDatagram(ENVIAR);
             UserDatagram privadoC = new UserDatagram(PRIVADO);
+            UserDatagram recibirC = new UserDatagram(RECIBIR);
 
             enviarC.start();
             privadoC.start();
+            recibirC.start();
 
             enviarC.join();
             privadoC.join();
+            recibirC.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -45,6 +69,9 @@ public class UserDatagram extends Thread {
                 break;
             case ENVIAR:
                 enviarMensajePrivado();
+                break;
+            case RECIBIR:
+                runRecibir();
                 break;
         }
     }
@@ -90,19 +117,46 @@ public class UserDatagram extends Thread {
         try {
             DatagramSocket datagramSocket = new DatagramSocket();
             InetAddress dirservidor = InetAddress.getByName("192.168.56.1");
-            Scanner scanner = new Scanner(System.in);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String mensaje;
 
             while (ejecucion) {
-                String mensaje = scanner.nextLine();
-                DatagramPacket datagrama4 = new DatagramPacket(mensaje.getBytes(),
-                        mensaje.getBytes().length, dirservidor, 7668);
-                datagramSocket.send(datagrama4);
+                mensaje = bufferedReader.readLine();
+                String[] msg =  mensaje.split(":");
+                if(mensaje.startsWith("Privado")) {
+                    DatagramPacket datagrama4 = new DatagramPacket(msg[1].getBytes(),
+                            msg[1].getBytes().length, dirservidor, 7668);
+                    datagramSocket.send(datagrama4);
+                }
+                else{
+                    mensaje = nombre + ":" + mensaje;
+                    DatagramPacket paquete = new DatagramPacket(mensaje.getBytes(), mensaje.length(), grupo.getAddress(), puerto);
+                    socketMulticast.send(paquete);
+                }
+
             }
             datagramSocket.close();
             System.out.println("Finalizando envío de mensajes.");
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    //Multicast
+    private void runRecibir() {
+        final int tamanoBufferMensaje = 1024;
+        String mensaje;
+        byte[] bufferMensajeRecibido = new byte[tamanoBufferMensaje];
+
+        while (ejecucion) {
+            try {
+                DatagramPacket paquete = new DatagramPacket(bufferMensajeRecibido, bufferMensajeRecibido.length);
+                socketMulticast.receive(paquete);
+                mensaje = new String(paquete.getData(), 0, paquete.getLength());
+                System.out.println(mensaje);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
